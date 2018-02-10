b7f64ff81897868e7c5c493fac189ffea3d08b65
kalviskin <kalviskin@r7.com>
2018-02-10 21:45:18 -0200

    Ready for deployment to heroku.

---
+++ learning_logs/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Topic(models.Model):
+	"""A subject which the user is learning."""
+	text = models.CharField(max_length=200)
+	date_added = models.DateTimeField(auto_now_add=True)
+	owner = models.ForeignKey(User, on_delete=models.CASCADE)
+	
+	def __str__(self):
+		"""Returns a string representation of the model."""
+		return self.text
+		
+class Entry(models.Model):
+	"""Something especific learned about something."""
+	topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+	text = models.TextField()
+	date_added = models.DateTimeField(auto_now_add=True)
+	
+	class Meta:
+		verbose_name_plural = 'entries'
+		
+		def __str__(self):
+			"""Returns a string representation model."""
+			if len(self.text) > 50:
+				return self.text[:50] + "..."
+			return self.text
---
+++ learning_logs/topic_owner.py
@@ -0,0 +1,7 @@
+from django.http import Http404
+
+def check_topic_owner(request, topic):
+	if topic.owner != request.user:
+		raise Http404
+			
+	
